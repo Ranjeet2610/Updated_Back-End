d7824075b09f443f7b5c8832bc149654f4c98946
Harendra Sharma <harendrasharma337@gmail.com>
2021-02-05 19:44:11 +0530

    Fancy Wala updated done

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -674,7 +674,7 @@ exports.placeFancyBet = (req, res) => {
                     return res.send({ status: false, message: "Technical Error" })
                 }
                 DB.user.findOne({ userName: req.body.userName }).then((user) => {
-                    user.walletBalance = parseFloat(user.walletBalance) - parseFloat(user.stack)
+                    user.walletBalance = parseFloat(user.walletBalance) - parseFloat(req.body.stack)
                     user.exposure = parseFloat(user.exposure) + parseFloat(req.body.loss)
                     user.save().then((updatedUser) => {
                         return res.send({
